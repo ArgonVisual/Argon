ff2785da0dd43fee3972910ae9b7526dccdd4ceb
GamerBillion <85514225+GamerBillion@users.noreply.github.com>
2022-07-07 14:55:25 -0400

    Items get shown in the solution tree view

---
+++ ArgonVisualX2/ArgonTreeItem.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Xps.Serialization;
+
+namespace ArgonVisualX2;
+public abstract class ArgonTreeItem : IComparable<ArgonTreeItem>
+{
+    public string HeaderName { get; set; }
+
+    public abstract ImageSource Icon { get; }
+
+    public List<ArgonTreeItem> TreeItems { get; }
+
+    public ArgonTreeItem(string headerName) 
+    {
+        HeaderName = headerName;
+        TreeItems = new List<ArgonTreeItem>();
+    }
+
+    public int CompareTo(ArgonTreeItem? other)
+    {
+        if (other is not null)
+        {
+            return HeaderName.CompareTo(other.HeaderName);
+        }
+
+        return 0;
+    }
+}
+
+
+
+public class ArgonSolutionTreeItem : ArgonTreeItem
+{
+    private static ImageSource _icon = new BitmapImage(new Uri("/ArgonVisualX2;component/Icons/ArgonProject.png", UriKind.Relative));
+    public override ImageSource Icon => _icon;
+
+    public ArgonSolutionTreeItem(string headerName) : base(headerName)
+    {
+        HeaderName = headerName;
+    }
+    
+}
+
+public class ArgonProjectTreeItem : ArgonTreeItem
+{
+    private static ImageSource _icon = new BitmapImage(new Uri("/ArgonVisualX2;component/Icons/ArgonProject.png", UriKind.Relative));
+    public override ImageSource Icon => _icon;
+
+    public ArgonProjectTreeItem(string headerName) : base(headerName)
+    {
+
+    }
+}
+
+public class ArgonFolderTreeItem : ArgonTreeItem
+{
+    private static ImageSource _icon = new BitmapImage(new Uri("/ArgonVisualX2;component/Icons/Folder.png", UriKind.Relative));
+    public override ImageSource Icon => _icon;
+
+    public ArgonFolderTreeItem(string headerName) : base(headerName)
+    {
+
+    }
+}
+
+public class ArgonCodeFileTreeItem : ArgonTreeItem
+{
+    private static ImageSource _icon = new BitmapImage(new Uri("/ArgonVisualX2;component/Icons/CodeFile.png", UriKind.Relative));
+    public override ImageSource Icon => _icon;
+
+    public ArgonCodeFileTreeItem(string headerName) : base(headerName)
+    {
+
+    }
+}
--- ArgonVisualX2/Views/SolutionView.xaml.cs
+++ ArgonVisualX2/Views/SolutionView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,9 +22,28 @@ public partial class SolutionView : UserControl
 {
     public string SolutionName { get; set; }
 
+    public List<ArgonTreeItem> TreeItems { get; }
+
     public SolutionView()
     {
         InitializeComponent();
         this.DataContext = this;
+
+        TreeItems = new List<ArgonTreeItem>();
+
+        TreeItems.Add(new ArgonProjectTreeItem("Minecraft"));
+
+        ArgonProjectTreeItem fortniteTreeItem = new ArgonProjectTreeItem("Fortnite");
+
+        fortniteTreeItem.TreeItems.Add(new ArgonCodeFileTreeItem("Battle Royale"));
+        fortniteTreeItem.TreeItems.Add(new ArgonCodeFileTreeItem("Creative"));
+        fortniteTreeItem.TreeItems.Add(new ArgonCodeFileTreeItem("Save the World"));
+
+        TreeItems.Add(fortniteTreeItem);
+        TreeItems.Add(new ArgonProjectTreeItem("Valorant"));
+        TreeItems.Add(new ArgonProjectTreeItem("Argon"));
+        TreeItems.Add(new ArgonFolderTreeItem("Roblox"));
+
+        TreeItems.Sort();
     }
 }
