657a0898e2e4cdf3d83f7cc576bcd1a696089361
GamerBillion <85514225+GamerBillion@users.noreply.github.com>
2022-07-11 14:05:42 -0400

    Actually figuring out how templates and styles work

---
+++ TreeViewTest/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TreeViewTest;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public List<TreeItem> TreeItems { get; }
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        TreeItems = new List<TreeItem>();
+
+        TreeItem solution = new TreeItem("Solution");
+        TreeItem folder = new TreeItem("Folder");
+        folder.TreeItems.Add(new TreeItem("Project"));
+        folder.TreeItems.Add(new TreeItem("Project"));
+        solution.TreeItems.Add(folder);
+        solution.TreeItems.Add(new TreeItem("Project"));
+
+        TreeItems.Add(solution);
+    }
+}
+
+public class TreeItem 
+{
+    public string Name { get; set; }
+
+    public List<TreeItem> TreeItems { get; }
+
+    public bool IsExpanded { get; set; }
+
+    public TreeItem(string name) 
+    {
+        Name = name;
+        TreeItems = new List<TreeItem>();
+        IsExpanded = true;
+    }
+}
